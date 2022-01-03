54d246ff8531a83315728e405b9cedd72bb23478
Marcel Greter <marcel.greter@ocbnet.ch>
2022-01-03 14:57:19 +0100

    Fix width of last pinned recipe

--- Harmony/PinRecipes.cs
+++ Harmony/PinRecipes.cs
@@ -13,8 +13,8 @@ public class PinRecipes : IModApi
 
     public void InitMod(Mod mod)
     {
-        Log.Out(" Loading Patch: " + this.GetType().ToString());
-        var harmony = new HarmonyLib.Harmony(GetType().ToString());
+        Log.Out(" Loading Patch: " + GetType().ToString());
+        Harmony harmony = new Harmony(GetType().ToString());
         harmony.PatchAll(Assembly.GetExecutingAssembly());
     }
 
