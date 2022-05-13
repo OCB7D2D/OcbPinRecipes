d908ca5c638ba9fddfba7442b6bdee366a431887
Marcel Greter <marcel.greter@ocbnet.ch>
2022-05-13 02:29:57 +0200

    Add conditional patcher to embed smx compatibility patch

--- 00-CompileModule.bat
+++ 00-CompileModule.bat
@@ -1,6 +1,8 @@
 @echo off
 
-call MC7D2D PinRecipes.dll /reference:"%PATH_7D2D_MANAGED%\Assembly-CSharp.dll" Harmony\*.cs && ^
+call MC7D2D PinRecipes.dll ^
+/reference:"%PATH_7D2D_MANAGED%\Assembly-CSharp.dll" ^
+Harmony\*.cs Dependency\*.cs && ^
 echo Successfully compiled PinRecipes.dll
 
 pause
---
+++ Dependency/ModXmlPatcher.cs
@@ -0,0 +1,272 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+
+static class ModXmlPatcher
+{
+
+    // Must be set from outside first, otherwise not much happens
+    public static Dictionary<string, Func<bool>> Conditions = null;
+
+    // Evaluates one single condition (can be negated)
+    private static bool EvaluateCondition(string condition)
+    {
+        // Try to get optional condition from global dictionary
+        if (Conditions != null && Conditions.TryGetValue(condition, out Func<bool> callback))
+        {
+            // Just call the function
+            // We don't cache anything
+            return callback();
+        }
+        // Otherwise check if a mod with that name exists
+        else if (ModManager.GetMod(condition) != null)
+        {
+            return true;
+        }
+        // Otherwise it's false
+        // Unknown tests too
+        return false;
+    }
+
+    // Evaluate a comma separated list of conditions
+    // The results are logically `and'ed` together
+    private static bool EvaluateConditions(string conditions)
+    {
+        // Ignore if condition is empty or null
+        if (string.IsNullOrEmpty(conditions)) return false;
+        // Split comma separated list (no whitespace allowed yet)
+        foreach (string condition in conditions.Split(','))
+        {
+            // Check if condition is negated
+            if (condition[0] == '!')
+            {
+                // If condition returns true, and group is false
+                if (EvaluateCondition(condition.Substring(1)))
+                {
+                    return false;
+                }
+            }
+            // Group is false if one condition is false
+            else if (!EvaluateCondition(condition))
+            {
+                return false;
+            }
+        }
+        // Something was true
+        return true;
+    }
+
+    // We need to call into the private function to proceed with XML patching
+    private static MethodInfo MethodSinglePatch = AccessTools.Method(typeof(XmlPatcher), "singlePatch");
+
+    // Function to load another XML file and basically call the same PatchXML function again
+    private static bool IncludeAnotherDocument(XmlFile target, XmlFile parent, XmlElement element, string modName)
+    {
+        bool result = true;
+        foreach (XmlAttribute attr in element.Attributes)
+        {
+            // Skip unknown attributes
+            if (attr.Name != "path") continue;
+            // Load path relative to previous XML include
+            string prev = Path.Combine(parent.Directory, parent.Filename);
+            string path = Path.Combine(Path.GetDirectoryName(prev), attr.Value);
+            if (File.Exists(path))
+            {
+                try
+                {
+                    string _text = File.ReadAllText(path, Encoding.UTF8);
+                    // .Replace("@modfolder:", "@modfolder(" + loadedMod.ModInfo.Name?.ToString() + "):");
+                    XmlFile _patchXml;
+                    try
+                    {
+                        _patchXml = new XmlFile(_text,
+                            Path.GetDirectoryName(path),
+                            Path.GetFileName(path),
+                            true);
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error("XML loader: Loading XML patch include '{0}' from mod '{1}' failed.", path, modName);
+                        Log.Exception(ex);
+                        result = false;
+                        continue;
+                    }
+                    result &= XmlPatcher.PatchXml(
+                        target, _patchXml, modName);
+                }
+                catch (Exception ex)
+                {
+                    Log.Error("XML loader: Patching '" + target.Filename + "' from mod '" + modName + "' failed.");
+                    Log.Exception(ex);
+                    result = false;
+                }
+            }
+            else
+            {
+                Log.Error("XML loader: Can't find XML include '{0}' from mod '{1}'.", path, modName);
+            }
+        }
+        return result;
+    }
+
+    // Basically the same function as `XmlPatcher.PatchXml`
+    // Patched to support `include` and `modif` XML elements
+    public static bool PatchXml(XmlFile xmlFile, XmlFile patchXml, XmlElement node, string patchName)
+    {
+        bool result = true;
+        foreach (XmlNode child in node.ChildNodes)
+        {
+            if (child.NodeType == XmlNodeType.Element)
+            {
+                if (!(child is XmlElement element)) continue;
+                // Patched to support includes
+                if (child.Name == "include")
+                {
+                    // Will do the magic by calling our functions again
+                    IncludeAnotherDocument(xmlFile, patchXml, element, patchName);
+                }
+                // Otherwise try to apply the patches found in child element
+                else if (!ApplyPatchEntry(xmlFile, patchXml, element, patchName))
+                {
+                    IXmlLineInfo lineInfo = (IXmlLineInfo)element;
+                    Log.Warning(string.Format("XML patch for \"{0}\" from mod \"{1}\" did not apply: {2} (line {3} at pos {4})",
+                        xmlFile.Filename, patchName, element.GetElementString(), lineInfo.LineNumber, lineInfo.LinePosition));
+                    result = false;
+                }
+            }
+        }
+        return result;
+    }
+
+    // Entry point from main harmony hook replacing `XmlPatcher.PatchXml`
+    public static bool PatchXml(XmlFile _xmlFile, XmlFile _patchXml, string _patchName)
+    {
+        XmlElement element = _patchXml.XmlDoc.DocumentElement;
+        if (element == null) return false;
+        return PatchXml(_xmlFile, _patchXml, element, _patchName);
+    }
+
+    // Flags for consecutive mod-if parsing
+    private static bool IfClauseParsed = false;
+    private static bool PreviousResult = false;
+
+    // Entry point instead of (private) `XmlPatcher.singlePatch`
+    // Implements conditional patching and also allows includes
+    private static bool ApplyPatchEntry(XmlFile _xmlFile, XmlFile _patchXml, XmlElement _patchElement, string _patchName)
+    {
+
+        // Only support root level
+        switch (_patchElement.Name)
+        {
+
+            case "include":
+
+                // Call out to our include handler
+                return IncludeAnotherDocument(_xmlFile, _patchXml,
+                    _patchElement, _patchName);
+
+            case "modif":
+
+                // Reset flags first
+                IfClauseParsed = true;
+                PreviousResult = false;
+
+                // Check if we have true conditions
+                foreach (XmlAttribute attr in _patchElement.Attributes)
+                {
+                    // Ignore unknown attributes for now
+                    if (attr.Name != "condition")
+                    {
+                        Log.Warning("Ignoring unknown attribute {0}", attr.Name);
+                        continue;
+                    }
+                    // Evaluate one or'ed condition
+                    if (EvaluateConditions(attr.Value))
+                    {
+                        PreviousResult = true;
+                        return PatchXml(_xmlFile, _patchXml,
+                            _patchElement, _patchName);
+                    }
+                }
+
+                // Nothing failed!?
+                return true;
+
+            case "modelsif":
+
+                // Check for correct parser state
+                if (!IfClauseParsed)
+                {
+                    Log.Error("Found <modelsif> clause out of order");
+                    return false;
+                }
+
+                // Abort else when last result was true
+                if (PreviousResult) return false;
+
+                // Reset flags first
+                PreviousResult = false;
+
+                // Check if we have true conditions
+                foreach (XmlAttribute attr in _patchElement.Attributes)
+                {
+                    // Ignore unknown attributes for now
+                    if (attr.Name != "condition")
+                    {
+                        Log.Warning("Ignoring unknown attribute {0}", attr.Name);
+                        continue;
+                    }
+                    // Evaluate one or'ed condition
+                    if (EvaluateConditions(attr.Value))
+                    {
+                        PreviousResult = true;
+                        return PatchXml(_xmlFile, _patchXml,
+                            _patchElement, _patchName);
+                    }
+                }
+
+                // Nothing failed!?
+                return true;
+
+            case "modelse":
+
+                // Abort else when last result was true
+                if (PreviousResult) return false;
+
+                // Reset flags first
+                IfClauseParsed = false;
+                PreviousResult = false;
+
+                return PatchXml(_xmlFile, _patchXml,
+                    _patchElement, _patchName);
+
+            default:
+                // Reset flags first
+                IfClauseParsed = false;
+                PreviousResult = true;
+                // Dispatch to original function
+                return (bool)MethodSinglePatch.Invoke(null,
+                    new object[] { _xmlFile, _patchElement, _patchName });
+        }
+    }
+
+    // Hook into vanilla XML Patcher
+    [HarmonyPatch(typeof(XmlPatcher))]
+    [HarmonyPatch("PatchXml")]
+    public class XmlPatcher_PatchXml
+    {
+        static bool Prefix(XmlFile _xmlFile, XmlFile _patchXml, string _patchName, ref bool __result)
+        {
+            // Call out to static helper function
+            __result = ModXmlPatcher.PatchXml(
+                _xmlFile, _patchXml, _patchName);
+            // Last one wins
+            return false;
+        }
+    }
+
+}
